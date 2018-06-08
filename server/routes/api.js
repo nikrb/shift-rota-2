7156184548bbc662b5939f4b4fa70e0dce29dc0d
nikrb <nik_rb@yahoo.com>
2018-06-08 21:58:21 +0100

    move old unauthed routes to authed

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,34 @@
 const express = require('express');
+var multer = require( 'multer');
+var upload = multer({ dest: process.env.upload_directory});
+var parseRota = require( '../models/pdfRotaParser');
+
+const Shift = require( '../models/Shift');
 
 const router = new express.Router();
 
-router.get('/someroute', (req, res) => {
-  res.status(200).json({
-    message: "You're authorized to see this data."
-  });
+router.post( '/shift', (req,res) => Shift.find( req, res));
+router.put( '/shift', (req,res) => Shift.create( req, res));
+router.delete( '/shift', (req,res) => Shift.delete( req, res));
+
+router.post( '/upload', upload.single( 'pdf'), function( req, res){
+  console.log( "@POST api/upload filepath:", req.file.path);
+  console.log( "import_flag:", req.body.import_flag);
+  const import_flag = req.body.import_flag === 'true';
+  const filename = req.file.originalname;
+  const ext = filename.substr(filename.lastIndexOf('.')+1);
+  if( ext === "pdf"){
+    parseRota( req.file.path, import_flag)
+    .then( function( shifts){
+      res.send( { success: true, shifts});
+    })
+    .catch( function( err) {
+      console.error( "parseRota failed:", err);
+      res.send( { success: false, message: "failed to parse rota"});
+    });
+  } else {
+    res.send( { success: false, message:"Filetype not supported"});
+  }
 });
 
 module.exports = router;
