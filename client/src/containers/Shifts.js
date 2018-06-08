bddf5092eb4061f66ed2d8ad719f06e5176a4894
nikrb <nik_rb@yahoo.com>
2018-06-08 19:46:14 +0100

    remove old text response after rota import

--- client/src/containers/Shifts.js
+++ client/src/containers/Shifts.js
@@ -6,7 +6,6 @@ export default class Shifts extends React.Component {
   constructor(){
     super();
     this.state = {
-      shifts : [],
       shift_html: null,
       file : null,
       show_date : new moment()
@@ -26,6 +25,7 @@ export default class Shifts extends React.Component {
     .then(function(response) {
       console.log( "file upload response:", response);
       // output shifts as html
+      // TODO: fix date and time formats
       const shift_html = response.shifts.map(sh => (
         <tr key={sh.start_time}>
           <td>{sh.owner_name}</td>
@@ -34,7 +34,7 @@ export default class Shifts extends React.Component {
           <td>{sh.end_time}</td>
         </tr>
       ));
-      that.setState( { shifts : response.shifts, shift_html});
+      that.setState( { shift_html });
     })
     .catch( function(err) {
       console.error( "file upload error:", err);
@@ -45,17 +45,6 @@ export default class Shifts extends React.Component {
   };
   render() {
     const { shift_html } = this.state;
-    const textarea_style = {
-      width: "100%"
-    };
-    // const days_in_month = this.state.show_date.daysInMonth();
-    // let shift_table = "";
-    // FIXME: in order to show a ShiftTable we need to organise the shifts
-    // into day and night and fill in missing shifts with slot_date member
-    // if( this.state.shifts.length){
-    //   shift_table = <ShiftTable show_date={this.state.show_date} shifts={this.state.shifts}
-    //               days_in_month={days_in_month} />;
-    // }
     return (
       <div className="container text-center rota-wrapper">
         <h4>Rota Import</h4>
@@ -79,9 +68,6 @@ export default class Shifts extends React.Component {
             </table>
             : <div>No Shifts</div>
           }
-        </div>
-        <div>
-          <textarea style={textarea_style} value={JSON.stringify( this.state.shifts)} readOnly></textarea>
         </div>
       </div>
     );
