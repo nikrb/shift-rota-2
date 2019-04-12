2bb9cc62294cc9b0da08d968527f8d24f73c8e42
nikrb <nik_rb@yahoo.com>
2019-04-12 18:42:14 +0100

    add PERS client to seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -10,7 +10,8 @@ MongoClient.connect(url, function(err, db) {
   db.collection('users').insertMany([
     // { name: "Nickolas Scott", initials: "NS", email:"nik_rb@yahoo.com", role:"worker" },
     { name: "Jeffrey Wilson",initials: "JW", email:"jw@noemail", role:"client" },
-    { name: "Stephen Morley",initials: "SM", email:"sm@noemail", role:"client" }
+    { name: "Stephen Morley",initials: "SM", email:"sm@noemail", role:"client" },
+    { name : "Personal Nik", initials : "PERS", email: "none@email.com", role : "client" },
   ]).then(results => {
     console.log(results);
     db.close();
